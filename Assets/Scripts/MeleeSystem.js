a074eb806ca3c46dd48acf7c225c5e6468890037
newxsaint <isaacrcelaya@gmail.com>
2014-03-13 21:59:01 -0700

    init

---
+++ Assets/Scripts/MeleeSystem.js
@@ -0,0 +1,22 @@
+#pragma strict
+var TheDammage : int = 50;
+var Distance : float;
+var MaxDistance : float = 1.5;
+var TheMace : Transform;
+
+function Update ()
+{	if (Input.GetButtonDown("Fire1"))
+	{
+	TheMace.animation.Play("Attack");
+	var hit : RaycastHit;
+		if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit ))
+		{
+			Distance = hit.distance;
+			if (Distance < MaxDistance)
+			{
+				hit.transform.SendMessage("ApplyDammage", TheDammage, SendMessageOptions.DontRequireReceiver);
+			}
+			
+		}	
+	}
+}
